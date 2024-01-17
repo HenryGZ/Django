209ee60b20a11bf37e3740c1b7a3ab540416c267
Henry Goes Zanin <henrygzanin@gmail.com>
2024-01-17 00:14:06 -0300

    fix - Refactor HTML and CSS code

--- Django 1º/blog/views.py
+++ Django 1º/blog/views.py
@@ -1,9 +1,11 @@
-
+from typing import Any
 from blog.data import posts
+from django.http import HttpRequest
 from django.shortcuts import render
 
 
 def blog(request):
+    print('blog')
 
     context = {
         # 'text': 'Olá blog',
@@ -16,26 +18,33 @@ def blog(request):
         context
     )
 
-def post(request, post_id):
 
-    found_post = None
-    
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = None
+
     for post in posts:
         if post['id'] == post_id:
             found_post = post
             break
 
+    if found_post is None:
+        raise Exception('Post não existe.')
+
     context = {
-        'posts': [found_post]
+        # 'text': 'Olá blog',
+        'post': found_post,
+        'title': found_post['title'] + ' - ',
     }
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post.html',
         context
     )
 
+
 def exemplo(request):
+    print('exemplo')
 
     context = {
         'text': 'Olá exemplo',
