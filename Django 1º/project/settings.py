65d8be2fb71e3725ff0079147a80d16dde79317f
Henry Goes Zanin <henrygzanin@gmail.com>
2024-01-18 02:12:57 -0300

    feat - pasta static files para produção

--- Django 1º/project/settings.py
+++ Django 1º/project/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rql=j-)trb_&@4*i737a5jw9!x*9wr&n7tb_u0b-q1vi(8lmn('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #mudar para false quando for para produção
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+    ] #mudar para o ip do servidor quando for para produção, usando o local host para testes
 
 
 # Application definition
@@ -123,6 +125,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base' / 'static'
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
