6a7dfd8af618eb55636a463c160e5c9a05613e44
Henry Goes Zanin <henrygzanin@gmail.com>
2024-01-18 23:27:30 -0300

    fix - Add ContactAdmin class to admin.py and update created_date field in models.py

--- Agenda/contact/models.py
+++ Agenda/contact/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -23,7 +24,10 @@ class Contact(models.Model):
     last_name = models.CharField(max_length=100, blank=False, null=False)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField(blank=True, null=True)
-    created_date = models.DateField(auto_now_add=True)#auto_now_add=True -> data de criação
+    created_date = models.DateTimeField(default=timezone.now)#auto_now_add=True -> data de criação
     description = models.TextField(blank=True, null=True)
     show = models.BooleanField(default=True)
     #picture = models.ImageField(upload_to='contact_pictures', blank=True, null=True)
+    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name #retorna o nome do contato parqa aparecer na página de admin do django
