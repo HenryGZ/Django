9589348a42421e7fae8bdbf864baa4a106f49136
Henry Goes Zanin <henrygzanin@gmail.com>
2024-01-21 17:39:11 -0300

    feat - sobrescrita de viwes para um modulo, afim de organizar melhor os views

---
+++ Agenda/contact/views/__init__.py
@@ -0,0 +1,12 @@
+#usado par ser a primeira coisa a ser executada quando esse pacote 
+#for importado e executado
+
+'''
+como esse arquivo é executado primeiro, as importaçãoes
+parecerão que são diretamente do diretório contact, mas na 
+verdade são do arquivoque está sendo importado
+'''
+from views.contact_views import *
+
+
+
