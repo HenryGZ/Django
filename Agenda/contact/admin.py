ee9103c58bdbd4a6a7b38ed0eb7aab260037ac49
Henry Goes Zanin <henrygzanin@gmail.com>
2024-01-22 11:16:38 -0300

    feat - diminuir os contatos mostrados e exibir apenas onde show = true

--- Agenda/contact/admin.py
+++ Agenda/contact/admin.py
@@ -10,13 +10,14 @@ class ContactAdmin(admin.ModelAdmin):
     '''existem muitas opções de configuração para o admin,
     para saber mais acesse: https://docs.djangoproject.com/en/5.0/ref/contrib/admin/'''
     
-    list_display = ('id','first_name','last_name','phone',) # Campos que serão exibidos na listagem
+    list_display = ('id','first_name','last_name','phone','show',) # Campos que serão exibidos na listagem
     ordering = ('id',) # Ordenação dos campos pelo id
     #search_fields = ('first_name','last_name','phone_number',) # Campos que serão pesquisados
     #list_filter = ('created_date',) # Campos que serão filtrados
     list_per_page = 30 # Quantidade de registros por página
     list_max_show_all = 100 # Quantidade máxima de registros a serem exibidos
     list_display_links = ('id','first_name',) # Campos que serão exibidos como links
+    list_editable = ('last_name','phone','show',) # Campos que serão editáveis na listagem
     
     @admin.register(models.Category)
     class CategoryAdmin(admin.ModelAdmin):
--- Agenda/contact/views/contact_views.py
+++ Agenda/contact/views/contact_views.py
@@ -3,7 +3,10 @@ from contact.models import Contact
 
 def index(request):
     
-    contacts = Contact.objects.all()
+    #pega os contatos da tabela onde o campo show é true e ordena por id
+    contacts = Contact.objects.filter(show=True).order_by('id')[:10]
+    
+    #print(contacts.query) mostra a query que está sendo executada
     
     context = {
         'contacts': contacts,
