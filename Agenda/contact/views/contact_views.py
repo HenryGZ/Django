b788faf93258f528e1ba84758ed444a36b27c620
Henry Goes Zanin <henrygzanin@gmail.com>
2024-01-22 13:38:10 -0300

    fix - tratamento de erro 404

--- Agenda/contact/views/contact_views.py
+++ Agenda/contact/views/contact_views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from contact.models import Contact
+from django.http import Http404
+
+from django.shortcuts import get_object_or_404
 
 def index(request):
     
@@ -21,8 +24,18 @@ def index(request):
 def contact(request, contact_id):
     
     #pega o contato da tabela onde o campo show é true e o id é igual ao id passado na url
-    single_contact = Contact.objects.get(pk = contact_id, show=True)
-        
+    #single_contact = Contact.objects.get(pk = contact_id, show=True)
+    
+    #o get pode trazer um problema de página não encontrada, então usamos o filter que retorna uma lista
+    #e pegamos o primeiro elemento da lista
+    # single_contact = Contact.objects.filter(pk = contact_id, show=True).first()
+    
+    # if single_contact is None:
+    #     raise Http404('Contato não encontrado')
+    
+    #pode ser feita assim, mas o django possui já um atalho para esse tipo de problema:
+    single_contact = get_object_or_404(Contact, pk = contact_id, show=True)
+    
     context = {
         'contact': single_contact,
     }
