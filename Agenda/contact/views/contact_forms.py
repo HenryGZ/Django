63f8e3139fe5093d102ace1b1bf5c931044be9b4
Henry Goes Zanin <henrygzanin@gmail.com>
2024-01-23 15:35:28 -0300

    feat - distinção de métodos na view

--- Agenda/contact/views/contact_forms.py
+++ Agenda/contact/views/contact_forms.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
+from django import forms
+from contact.models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'phone',)
 
 def create(request):
-    context= {
-    
-    }
-    
-    return render(
-        request,
-        'contact/create.html',
-        context
-        )
+    if request.method == 'POST':
+        context = {
+            'form' : ContactForm(request.POST)
+        }
+        return render(
+            request,
+            'contact/create.html',
+            context
+            )
+    else:  
+        context = {
+            'form' : ContactForm(),
+        }
+        return render(
+            request,
+            'contact/create.html',
+            context
+            )
