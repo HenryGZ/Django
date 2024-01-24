2603977442cdaedef4e9c3aa09a70054b0f6de61
Henry Goes Zanin <henrygzanin@gmail.com>
2024-01-24 20:20:34 -0300

    feat - salvamento de novos contatos

--- Agenda/contact/forms.py
+++ Agenda/contact/forms.py
@@ -68,7 +68,12 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = (
-            'first_name', 'last_name', 'phone',
+            'first_name', 
+            'last_name', 
+            'phone',
+            'email',
+            'description',
+            'category',
         )
         
     
--- Agenda/contact/views/contact_forms.py
+++ Agenda/contact/views/contact_forms.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from contact.forms import ContactForm
+from django.shortcuts import redirect
 
 def create(request):
     if request.method == 'POST':
+        
+        form = ContactForm(request.POST)
+                
         context = {
             'form' : ContactForm(request.POST)
         }
+        
+        if form.is_valid():
+            # contact = form.save(commit=False) # commit=False para não salvar no banco de dados
+            form.save(commit=True)#commit=True para salvar no banco de dados, ele é true por defautl
+            return redirect('contact:create') 
+            
         return render(
             request,
             'contact/create.html',
@@ -15,6 +25,8 @@ def create(request):
         context = {
             'form' : ContactForm(),
         }
+        
+        
         return render(
             request,
             'contact/create.html',
