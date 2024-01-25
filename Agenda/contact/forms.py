f7137de96a048d8fd642f42c2abe7c0bebbbd3df
Henry Goes Zanin <henrygzanin@gmail.com>
2024-01-25 10:00:55 -0300

    feat - form de update

--- Agenda/contact/forms.py
+++ Agenda/contact/forms.py
@@ -54,7 +54,7 @@ widgets = {
     )
 }
 '''
-class ContactForm(forms.ModelForm):    
+class ContactForm(forms.ModelForm):
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
@@ -65,42 +65,55 @@ class ContactForm(forms.ModelForm):
         label='Primeiro Nome',
         help_text='Texto de ajuda para seu usuário',
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Aqui veio do init',
+        # })
+
     class Meta:
         model = Contact
         fields = (
-            'first_name', 
-            'last_name', 
-            'phone',
-            'email',
-            'description',
-            'category',
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
         )
-        
-    
-    def clean(self):
-        #clean geral que pode ser usado para validar campos entre si
-        first_name = self.cleaned_data['first_name']
-        last_name = self.cleaned_data['last_name']
-        
-        if first_name == last_name:
-            #raise ValidationError('Primeiro nome e último nome não podem ser iguais') #o raise ValidationError para a execução do clean
-            
-            msg = ValidationError('Primeiro nome e último nome não podem ser iguais', code='invalid')
-            
-            self.add_error('first_name', msg) 
-            self.add_error('last_name', msg)
-            
-        return super().clean()
-    
-    def clean_first_name(self):
-            first_name = self.cleaned_data.get('first_name')
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui',
+        #         }
+        #     )
+        # }
 
-            if first_name == 'ABC':
-                self.add_error(
-                    'first_name',
-                    ValidationError(
-                        'Veio do add_error',
-                        code='invalid'
-                    )
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+
+        return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
                 )
-            return first_name
+            )
+
+        return first_name
--- Agenda/contact/views/contact_forms.py
+++ Agenda/contact/views/contact_forms.py
@@ -1,34 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+
 from contact.forms import ContactForm
-from django.shortcuts import redirect
+from contact.models import Contact
+
 
 def create(request):
+    form_action = reverse('contact:create')
+
     if request.method == 'POST':
-        
         form = ContactForm(request.POST)
-                
+
         context = {
-            'form' : ContactForm(request.POST)
+            'form': form,
+            'form_action': form_action,
         }
-        
+
         if form.is_valid():
-            # contact = form.save(commit=False) # commit=False para não salvar no banco de dados
-            form.save(commit=True)#commit=True para salvar no banco de dados, ele é true por defautl
-            return redirect('contact:create') 
-            
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+
         return render(
             request,
             'contact/create.html',
             context
-            )
-    else:  
+        )
+
+    context = {
+        'form': ContactForm(),
+        'form_action': form_action,
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
+
+
+def update(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    form_action = reverse('contact:update', args=(contact_id,))
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+
         context = {
-            'form' : ContactForm(),
+            'form': form,
+            'form_action': form_action,
         }
-        
-        
+
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+
         return render(
             request,
             'contact/create.html',
             context
-            )
+        )
+
+    context = {
+        'form': ContactForm(instance=contact),
+        'form_action': form_action,
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
